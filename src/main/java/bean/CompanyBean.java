3b2f07fbcd38927a1a22f99c6ce40cc51f7cbf26
Muhammet Ali Balcı <aliumut2736@gmail.com>
2026-04-28 13:15:35 +0300

    Starting the company module, connecting the database.

---
+++ src/main/java/bean/CompanyBean.java
@@ -0,0 +1,56 @@
+package bean;
+
+import entity.Company;
+import facadeLocal.CompanyFacadeLocal;
+import jakarta.ejb.EJB;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+
+@Named
+@SessionScoped
+public class CompanyBean implements Serializable {
+
+    private Company company = new Company();
+
+    @EJB
+    private CompanyFacadeLocal companyFacade;
+
+    public void createCompany() {
+        companyFacade.create(company);
+        company = new Company();
+    }
+
+    public void updateCompany() {
+        companyFacade.edit(company);
+        company = new Company();
+    }
+
+    public void deleteCompany(Company selectedCompany) {
+        companyFacade.remove(selectedCompany);
+    }
+
+    public void selectCompany(Company selectedCompany) {
+        this.company = selectedCompany;
+    }
+
+    public void clearForm() {
+        this.company = new Company();
+    }
+
+    public List<Company> getCompanies() {
+        return companyFacade.findAll();
+    }
+
+    public Company getCompany() {
+        if (company == null) {
+            company = new Company();
+        }
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+}
---
+++ src/main/java/facade/CompanyFacade.java
@@ -0,0 +1,28 @@
+package facade;
+
+import entity.Company;
+import facadeLocal.CompanyFacadeLocal;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+@Stateless
+public class CompanyFacade extends AbstractFacade<Company> implements CompanyFacadeLocal {
+
+    @PersistenceContext(unitName = "default")
+    private EntityManager entityManager;
+
+    public CompanyFacade() {
+        super(Company.class);
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return entityManager;
+    }
+
+    @Override
+    public Company find(Long id) {
+        return null;
+    }
+}
